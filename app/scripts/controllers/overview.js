0e228b4d6ac6cc7e9b70717d7148ea2d4ff141d5
Jérôme Wax <nospam@lo2k.net>
2014-06-25 23:56:35 +0200

    WIP

--- app/scripts/controllers/overview.js
+++ app/scripts/controllers/overview.js
@@ -1,14 +1,24 @@
 'use strict';
 
 angular.module('v9App')
-  .controller('OverviewCtrl', function ($scope, $http, BalanceService, BudgetService) {
+  .controller('OverviewCtrl', function ($scope, $http, BalanceService, BudgetService, $filter) {
       var momentCursor = moment();
+      var numberFilter = $filter('number');
       $scope.balances = {};
       $scope.budgets = {};
 
-      $scope.computedBudgets = {}
+      $scope.computedBudgets = {};
 
-      var past = moment().subtract('months', 4);
+      $scope.monthInProgress = moment().format('YYYY/MM');
+
+     var past = momentCursor = moment().subtract('months', 4);
+
+      $scope.setSelectedMonth = function(selectedMonth) {
+        $scope.monthInProgress = selectedMonth;
+        $scope.refreshOverall();
+      }
+
+      $scope.getSelectedMonth = function() {return $scope.monthInProgress;}
 
       $scope.retrieveVarsFor = function(momentObj) {
             var indexedString = momentObj.format('YYYY/MM');
@@ -47,10 +57,33 @@ angular.module('v9App')
             _.each(value.tags, function(value2, key2, list2) {
               $scope.allTags.push(key2);
             });
+
+            $scope.refreshPrevisionForBudget(key,value);
+
+
          }  );
 
          $scope.allTags= _.uniq($scope.allTags);
         //console.log($scope.allTags);
+      }
+
+      $scope.refreshPrevisionForBudget = function( key, budget) {
+        console.log(key)
+        _.each(budget.tags, function(value, key2, list) {
+            //console.log(key > $scope.getSelectedMonth());
+            //console.log(key+" > "+$scope.getSelectedMonth());
+            value.displayNote = undefined;
+            if (key > $scope.getSelectedMonth()) {
+              value.displayVal = value.prevision;
+            } else if ( key == $scope.getSelectedMonth()) {
+              value.displayVal = -12;
+              value.displayNote = numberFilter(value.consumed,2)+" ("+numberFilter(value.prevision,2)+")";
+            } else {
+              value.displayVal = value.consumed;            
+            }
+        });
+
+        return budget;
       }
 
 
